1a8412bc31a733e807b21190858527035125fa34
TylerGrayStudent <42650829+TylerGrayStudent@users.noreply.github.com>
2023-05-01 11:44:42 -0400

    1 file modified, 2 added, 2 deleted

--- src/services/branch/branch.service.ts
+++ src/services/branch/branch.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { Branches } from 'src/data/Branches';
-import { Branch } from 'src/models/Branch';
 
 @Injectable({ providedIn: 'root' })
 export class BranchService {
