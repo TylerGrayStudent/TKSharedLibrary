7b2172ad36a4f5d6c849cf0f93d18414f8fbdc2e
TylerGrayStudent <42650829+TylerGrayStudent@users.noreply.github.com>
2023-05-01 12:40:32 -0400

    2 files modified

--- src/services/branch/branch.service.ts
+++ src/services/branch/branch.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
+import { Branch } from 'src/models/Branch';
+import { Branches } from 'src/data/Branches';
 @Injectable({ providedIn: 'root' })
 export class BranchService {
   allBranches$ = new BehaviorSubject<Branch[]>(Branches);
